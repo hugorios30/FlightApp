585f55d60098c35b0a166347690b565d7090fb79
hugorios30 <hugo.rios30@gmail.com>
2023-01-12 14:28:38 -0600

    added client folder and initial typescript and webpack setup

---
+++ client/webpack.config.js
@@ -0,0 +1,45 @@
+const prod = process.env.NODE_ENV === 'production';
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+    new LiveReloadPlugin({
+      protocol: 'http',
+      port: 35729,
+      hostname: 'localhost',
+      appendScriptTag: false,
+      ignore: [null],
+      delay: 0,
+      useSourceHash: false
+    })
+  ],
+};
+
